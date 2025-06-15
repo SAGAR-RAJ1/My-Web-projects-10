18ba1c12d7e79dc5504d2e8af2b71277608544e8
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-06-15 19:12:56 +0530

    Added function for matching sequence and to restart if lost

--- SimonSays_Game/app.js
+++ SimonSays_Game/app.js
@@ -12,6 +12,22 @@ document.addEventListener("keypress",()=>{
 levelup()
     }
 })
+function checkAns(index){
+    // let index= level-1;   fixed index need to change
+    if (userSeq[index]===gameSeq[index]) {
+        console.log("correct");
+        if(userSeq.length==gameSeq.length){
+             setTimeout(levelup,1000);
+        }
+    } else {
+        heading.innerText="Game Over! Press any key to start again" 
+        start=false;
+        gameSeq=[];
+        userSeq=[];
+        level=0;  
+    }
+
+}
 function btnFlash(btn){
 btn.classList.add("flash");
 setTimeout(() => {
@@ -25,12 +41,15 @@ setTimeout(() => {
 
 function levelup(){
     level++;
+    userSeq=[];
 
     heading.innerText=`Level ${level}`;
     let randomIndex = Math.floor(Math.random()*4);
     let randomColor=btns[randomIndex];
     let randomBtn = document.querySelector(`.${randomColor}`)
     // console.log(randomBtn);
+    gameSeq.push(randomColor);
+    console.log(gameSeq);
 btnFlash(randomBtn);
 
 }
@@ -39,7 +58,13 @@ function btnPress() {
     // console.log(this);
     let btn = this;
     btnFlashClick(btn);
+
+ userColor = btn.getAttribute("id");
+ console.log(userColor);
+ userSeq.push(userColor);
+ checkAns(userSeq.length-1);
 }
+
 let allbtns=document.querySelectorAll(".btn");
 
 for(btn of allbtns){
