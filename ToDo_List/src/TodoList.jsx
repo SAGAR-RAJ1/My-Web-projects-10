b4a76d7aecc44a14f29682baee22962338737213
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-06-18 18:36:33 +0530

    Added delete function

--- ToDo_List/src/TodoList.jsx
+++ ToDo_List/src/TodoList.jsx
@@ -6,13 +6,26 @@ export default function TodoList() {
   const [New, setNew] = useState("");
   function btnclick() {
     // console.log("button click to add todo")
-    Todo.push({task:New , id:uuidv4()})
-//Address re refrence krne k liye spread operator use krte h kuki change detect hoga react ko tb update krega
-// setTodo([...Todo,New])  aise v likh shkte {array or object same case pdega}
-    setTodo([...Todo]);
+    // Todo.push({task:New , id:uuidv4()})
+// //Address re refrence krne k liye spread operator use krte h kuki change detect hoga react ko tb update krega
+// // setTodo([...Todo,New])  aise v likh shkte {array or object same case pdega}
+//     setTodo([...Todo]);
+
+//Better method to do it
+setTodo((prevTodo)=>{
+    return [...prevTodo,{task:New,id:uuidv4()}]
+  })
+
     setNew("")
   }
-  
+  function DeleteTodo(key){
+   
+        // console.log(key)
+        let copy = Todo.filter((todo)=>(todo.id!=key))
+        setTodo(copy);
+    
+  }
+
   function HandleKeyPress(event) {
     if (event.key === "Enter") {
       btnclick();
@@ -38,7 +51,10 @@ export default function TodoList() {
       <h3>Tasks</h3>
       <ul>
         {Todo.map((a) => (
-          <li key={a.id}>{a.task}</li>
+          <li key={a.id}>
+            <span>{a.task} </span>
+            <button onClick={()=>DeleteTodo(a.id)}>Delete</button>
+            </li>
         ))}
       </ul>
     </div>
